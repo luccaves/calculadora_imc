d7780d20f2991b7c8632deb2abd0ab01e36b018f
luccaves <lucasworkmail26@gmail.com>
2026-06-08 23:09:41 -0300

    Implementação do sistema JOptionPane para inntereção com inputs visuais em gráficos

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,51 @@
-import java.util.Locale;
-import java.util.Scanner;
+import javax.swing.JOptionPane;
+//import java.util.Scanner;
 public class Main {
     // Declaracao de variaveis
     static double weight;
     static double height;
 
     // Declaração de objetos
-    static Scanner userinput = new Scanner(System.in);
+    //static Scanner userinput = new Scanner(System.in);
 
     /// Metodos
     //Calcula o IMC
     static void calculateIMC(double weight, double height) {
         double imc = weight / Math.pow(height, 2);
 
-        System.out.println("Seu IMC é de: " + imc);
+        String message = String.format("Seu IMC é de %.2f: ", imc);
         if (imc < 40) {
             if (imc < 18.5) {
-                System.out.println("Abaixo do peso");
+                JOptionPane.showMessageDialog(null, message + "Você esta abaixo do peso.");
+
             } else if (imc >= 18.6 && imc <= 24.9) {
-                System.out.println("Peso ideal (parabéns)");
+                JOptionPane.showMessageDialog(null, message + "Você esta no PESO IDEAL. Parabéns!!!");
+
             } else if (imc >= 25 && imc <= 29.9) {
+                JOptionPane.showMessageDialog(null, message + "Você esta LEVEMENTE acima do peso");
                 System.out.println("Levemente acima do peso");
+
             } else if (imc >= 30 && imc <= 34.9) {
-                System.out.println("Obesidade grau I");
+                JOptionPane.showMessageDialog(null, message + "Você um POUCO ACIMA do peso. Obesidade grau I");
+
             } else if (imc >= 35 && imc <= 39.9) {
-                System.out.println("Obesidade grau II (severa)");
+                JOptionPane.showMessageDialog(null, message + "Você esta MUITO ACIMA do peso. Obesidade grau II (severa)");
+
             }
         }else {
-            System.out.println("Obesidade III (mórbida)");
+            JOptionPane.showMessageDialog(null, message + "Você esta EXTREMAMENTE ACIMA do peso. Obesidade III (mórbida)");
         }
     }
 
 
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
 
         System.out.println("------------------------------------");
         System.out.println("        CALCULADORA DE IMC");
         System.out.println("------------------------------------");
-        System.out.println("Digite o seu peso:");
-        weight = userinput.nextDouble();
-        System.out.println("Digite sua altura:");
-        height = userinput.nextDouble();
+        weight = Double.parseDouble(JOptionPane.showInputDialog("Digite o seu peso:"));
+        height = Double.parseDouble(JOptionPane.showInputDialog("Digite sua altura:"));
         calculateIMC(weight, height);
+
         }
     }
